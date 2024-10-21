1619774ec99c8faf850792812f48e765854fd232
sinisti <spideralan2008@gmail.com>
2024-10-21 08:54:41 +0200

    Poprawienie Linijek

--- app/src/main/java/com/example/statemanagementapp/MainActivity.java
+++ app/src/main/java/com/example/statemanagementapp/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.statemanagementapp;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,9 +10,7 @@ import android.widget.CompoundButton;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Switch;
-
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private static final String KEY_COUNT = "count";
     private TextView textViewCount;
     private int count =0;
@@ -22,9 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox checkbox;
     private LinearLayout main;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         textViewCount=findViewById(R.id.text);
         Button buttonIncrement = findViewById(R.id.button);
@@ -32,17 +28,13 @@ public class MainActivity extends AppCompatActivity {
             count = savedInstanceState.getInt(KEY_COUNT);
         }
         updateCountText();
-
         buttonIncrement.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 count++;
                 updateCountText();
             }
-
         });
-
-        // Ustawienie słuchacza zmiany stanu
         switcher = findViewById(R.id.switcher);
         main = findViewById(R.id.main_layout);
         switcher.setOnCheckedChangeListener((buttonView, isChecked) -> {
@@ -52,24 +44,18 @@ public class MainActivity extends AppCompatActivity {
                 main.setBackgroundColor(Color.WHITE);
             }
         });
-
         checkbox = findViewById(R.id.check);
         textViewOpcjaZaznaczona = findViewById(R.id.inform);
-
-        // Nasłuchuj zmiany stanu CheckBoxa
         checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
                 if (isChecked) {
-                    // Jeśli zaznaczony, pokaż TextView
                     textViewOpcjaZaznaczona.setVisibility(View.VISIBLE);
                 } else {
-                    // Jeśli odznaczony, ukryj TextView
                     textViewOpcjaZaznaczona.setVisibility(View.GONE);
                 }
             }
         });
-
     }
     @Override
     protected  void onSaveInstanceState(Bundle outState){
@@ -79,5 +65,4 @@ public class MainActivity extends AppCompatActivity {
     protected void updateCountText(){
         textViewCount.setText("Licznik: "+ count);
     }
-
 }
